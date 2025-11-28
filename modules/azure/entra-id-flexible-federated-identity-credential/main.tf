85435185c46bc3deedaeadb91badd6ec3762f6fe
bemasek <99993104+bemasek@users.noreply.github.com>
2025-11-28 08:46:44 -0600

    updates

--- modules/azure/entra-id-flexible-federated-identity-credential/main.tf
+++ modules/azure/entra-id-flexible-federated-identity-credential/main.tf
@@ -20,10 +20,7 @@ resource "null_resource" "flexible_federated_identity_credential" {
   provisioner "local-exec" {
     when    = create
     command = <<EOT
-      az rest --method post --url 'https://graph.microsoft.com/beta/applications/f6d9593e-f9f8-4d9d-a1e9-52c5772f20e6/federatedIdentityCredentials' --headers 'Content-Type=application/json' --body "{\"audiences\":[\"api://AzureADTokenExchange\"],\"claimsMatchingExpression\":{\"languageVersion\":1,\"value\":\"claims['sub'] matches 'repo:GuggenheimInvestmentsPOC/IT-IaC-Azure:*'\"},\"issuer\":\"https://token.actions.githubusercontent.com\",\"name\":\"pipelines-plan\"}"
-      
-      # RAN MANUALLY
-      #az rest --method post --url 'https://graph.microsoft.com/beta/applications/f6d9593e-f9f8-4d9d-a1e9-52c5772f20e6/federatedIdentityCredentials' --headers 'Content-Type=application/json' --body "{'audiences':['api://AzureADTokenExchange'],'claimsMatchingExpression':{'languageVersion':1,'value':'claims[\'sub\'] matches \'repo:GuggenheimInvestmentsPOC/IT-IaC-Azure:*\''},'issuer':'https://token.actions.githubusercontent.com','name':'pipelines-plan'}"
+      az rest --method post --url 'https://graph.microsoft.com/beta/applications/${self.triggers.application_id}/federatedIdentityCredentials' --headers 'Content-Type=application/json' --body "{'audiences':['api://AzureADTokenExchange'],'claimsMatchingExpression':{'languageVersion':1,'value':'claims[\'sub\'] matches \'repo:GuggenheimInvestmentsPOC/IT-IaC-Azure:*\''},'issuer':'https://token.actions.githubusercontent.com','name':'${self.triggers.display_name}'}"
     EOT
   }
 
